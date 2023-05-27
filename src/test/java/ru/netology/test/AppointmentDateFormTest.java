fed68490cb3e428456fbe6366288573b56e8b697
Alexey <117473731+AlexeyKost@users.noreply.github.com>
2023-05-27 23:06:23 +0300

    Update AppointmentDateFormTest.java

--- src/test/java/ru/netology/test/AppointmentDateFormTest.java
+++ src/test/java/ru/netology/test/AppointmentDateFormTest.java
@@ -1,31 +1,39 @@
 package ru.netology.test;
+package ru.netology.test;
 
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Configuration.holdBrowserOpen;
+import static com.codeborne.selenide.Configuration.*;
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AppointmentDateFormTest {
 
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
     @Test
     void testAppointmentDate() {
+        String planningDate = generateDate(4);
 
         holdBrowserOpen = true;
         open("http://localhost:9999/");
         $("span[data-test-id=city] input").setValue("Краснодар");
         $("span[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
-        $("span[data-test-id=date] input").sendKeys("25.05.2023");
+        $("span[data-test-id=date] input").sendKeys(planningDate);
         $("span[data-test-id=name] input").setValue("Пупкин Иван");
         $("span[data-test-id=phone] input").setValue("+71234567890");
         $("label[data-test-id=agreement]").click();
         $x("//span[contains(text(), 'Забронировать')]").click();
         $("div[data-test-id=notification]")
-                .shouldHave(Condition.text("Встреча успешно забронирована на 25.05.2023"), Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
                 .shouldBe(Condition.visible);
 
     }
